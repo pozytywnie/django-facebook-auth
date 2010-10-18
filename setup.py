29ae88686e084a28aa7f56c07de8debd78d965b0
Tomasz Wysocki <tomasz@wysocki.info>
2010-10-18 08:59:33 +0200

    Adds setup.py.
    Change-Id:	I46122ce8d0614a19837dd682662446b3b8fd2542

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='django-facebook-auth',
+    version='0.1',
+    description="Authorisation app for Facebook API.",
+    maintainer="Tomasz Wysocki",
+    maintainer_email="tomasz@wysocki.info",
+
+    install_requires=(
+        'django>=1.2',
+        'facebook-python-sdk',
+        'simplejson',
+    ),
+
+    packages=[
+        'facebook_auth',
+    ],
+)
+
