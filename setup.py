9f1d2458a57207fa6620658af1db627f906416af
Tomasz Wysocki <tomasz@wysocki.info>
2012-07-18 11:40:46 +0200

    Bump version.
    Change-Id: Ib9e03eae0d149fdd9649d159a39b23fa787ca70a

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(name):
 
 setup(
     name='django-facebook-auth',
-    version='2.5',
+    version='2.6',
     description="Authorisation app for Facebook API.",
     long_description=read("README.rst"),
     maintainer="Tomasz Wysocki",
