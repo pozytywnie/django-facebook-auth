69878ba8fb15e3575aea9482156ef19ad0ed3efa
Tomasz Wysocki <tomasz@wysocki.info>
2012-08-17 12:03:43 +0200

    Bump version.
    Change-Id: Ia136ccd098316de5752153ed6e7ef070acea336e
    Reviewed-on: http://review.pozytywnie.pl:8080/5742
    Tested-by: Jenkins
    Reviewed-by: Tomasz Wysocki <tomwys@gmail.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(name):
 
 setup(
     name='django-facebook-auth',
-    version='2.6.3',
+    version='2.7',
     description="Authorisation app for Facebook API.",
     long_description=read("README.rst"),
     maintainer="Tomasz Wysocki",
