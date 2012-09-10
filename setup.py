d1ef8c50a13b5929bce4461cb94f6e29f016fc66
Piotr Maliński <piotr@pozytywnie.pl>
2012-09-10 15:51:11 +0200

    Bump version to 2.7.1.
    Change-Id: I25a9da7eb9f4f920f6c7062eadff536415dbd14f
    Reviewed-on: http://review.pozytywnie.pl:8080/6232
    Tested-by: Jenkins
    Reviewed-by: Tomasz Wysocki <tomwys@gmail.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(name):
 
 setup(
     name='django-facebook-auth',
-    version='2.7',
+    version='2.7.1',
     description="Authorisation app for Facebook API.",
     long_description=read("README.rst"),
     maintainer="Tomasz Wysocki",
