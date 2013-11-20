6baa78b5cd7a7a93a1f202198fad5b7171f13f6d
Piotr Maliński <piotr@pozytywnie.pl>
2013-11-20 09:46:23 +0100

    Fix migration.
    Change-Id: I0d3246471ca6d7f4b39b83a092b3a304d733f149
    Reviewed-on: http://review.pozytywnie.pl:8080/19728
    Tested-by: Jenkins
    Reviewed-by: Tomasz Wysocki <tomasz@pozytywnie.pl>

--- facebook_auth/migrations/0009_auto__add_unique_usertoken_token.py
+++ facebook_auth/migrations/0009_auto__add_unique_usertoken_token.py
@@ -9,7 +9,7 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         for token in orm['facebook_auth.UserToken'].objects.all():
-            orm['facebook_auth.UserToken'].objects.filter(token=token, id__gt=token.id).delete()
+            orm['facebook_auth.UserToken'].objects.filter(token=token.token, id__gt=token.id).delete()
         db.commit_transaction()
         db.start_transaction()
         # Adding unique constraint on 'UserToken', fields ['token']
