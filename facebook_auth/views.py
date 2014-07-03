270a03dc78838137051fec49050f550c44be2359
Tomasz Pazurkiewicz <tomasz.pazurkiewicz@pozytywnie.pl>
2014-07-03 09:35:56 +0200

    Add facebook error handler in view.
    This assumes that there is no other backend which
    can authenticate user with facebook credentials.

--- facebook_auth/views.py
+++ facebook_auth/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login
 from django import http
 from django.views import generic
+
+import facepy
+
 from facebook_auth import urls
 
 
@@ -13,26 +16,32 @@ logger = logging.getLogger(__name__)
 class Handler(generic.View):
     def get(self, request):
         try:
-            next_url = urls.Next().decode(request.GET['next'])
+            self.next_url = urls.Next().decode(request.GET['next'])
         except urls.InvalidNextUrl:
             logger.warning('Invalid facebook handler next.',
                            extra={'request': request})
             return http.HttpResponseBadRequest()
         if 'code' in request.GET:
-            self.login(next_url)
-            response = http.HttpResponseRedirect(next_url['next'])
+            try:
+                self.login()
+            except facepy.FacepyError as e:
+                return self.handle_facebook_error(e)
+            response = http.HttpResponseRedirect(self.next_url['next'])
             response["P3P"] = ('CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi'
                                ' IVDi CONi HIS OUR IND CNT"')
         else:
-            response = http.HttpResponseRedirect(next_url['close'])
+            response = http.HttpResponseRedirect(self.next_url['close'])
         return response
 
-    def login(self, next_url):
+    def login(self):
         user = authenticate(
             code=self.request.GET['code'],
-            redirect_uri=urls.redirect_uri(next_url['next'],
-                                           next_url['close']))
+            redirect_uri=urls.redirect_uri(self.next_url['next'],
+                                           self.next_url['close']))
         if user:
             login(self.request, user)
+
+    def handle_facebook_error(self, e):
+        return http.HttpResponseRedirect(self.next_url['next'])
 
 handler = Handler.as_view()
